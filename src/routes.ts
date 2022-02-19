1b3399be243c28d2fec3c4627f6713a16638191d
luanfv <luanfrancavieira@gmail.com>
2022-02-19 13:54:05 -0300

    feat: create routes and db of the user and messages

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,20 @@
 import { Router } from 'express';
 
 import { SettingsController } from './controllers/SettingsController';
+import { UsersController } from './controllers/UsersController';
+import { MessagesController } from './controllers/MessagesController';
 
 const routes = Router();
 
 const settingsControler = new SettingsController();
+const usersController = new UsersController();
+const messagesController = new MessagesController();
 
 routes.post('/settings', settingsControler.create);
+
+routes.post('/users', usersController.create);
+
+routes.post('/messages', messagesController.create);
+routes.get('/messages/:id', messagesController.showByUser);
 
 export { routes };
